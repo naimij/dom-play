b51a14d0e202876f569621e33445bb24edc9ecac
jawad naimi <jdnaimi@fake.com>
2022-12-15 19:03:51 -0800

    added some javaScript

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
+let actors = document.querySelectorAll(".hamlet");
+ 
+console.log(actors);
+ 
+for (let i = 0; i < actors.length; i++) {
+ actors[i].style.backgroundColor = "orange";
+}
+
+
+
+
 //https://tinyurl.com/dynamic-html-checker
 document.getElementById("html-checker").setAttribute("href","https://validator.w3.org/nu/?doc=" + location.href);
     
